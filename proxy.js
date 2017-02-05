f40cfedfccc0c0e107bcd7068c728f5be078242b
ultra2 <user10000@freemail.hu>
2017-02-06 00:22:11 +0100

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -104,7 +104,7 @@ class Server {
                 err.status = 404;
                 next(err);
             });
-            var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8081;
+            var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 4000;
             this.server.listen(port, function () {
                 console.log('Proxy listen on %s ...', port);
             });
